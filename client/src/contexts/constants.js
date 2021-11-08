cd35aaaf59f095e120d5100218195f141aec004a
VanHungNguyen <vanhungnguyenbku@gmail.com>
2021-11-08 12:05:47 +0700

    .

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
 		? 'http://localhost:3001/api'
-		: 'somedeployURL'
+		: 'https://vanhungnguyen-learnit.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern'
 
